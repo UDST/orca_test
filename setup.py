30c5157213e5a285563e6336cd405e628fd3e62d
Sam Maurer <maurer@berkeley.edu>
2018-02-16 15:18:05 -0800

    Simplifying setup script

--- setup.py
+++ setup.py
@@ -1,22 +1,9 @@
-# Install setuptools if not installed.
-try:
-    import setuptools
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-
 from setuptools import setup, find_packages
-
-
-# read README as the long description
-with open('README.md', 'r') as f:
-    long_description = f.read()
 
 setup(
     name='orca_test',
-    version='0.1dev',
+    version='0.1',
     description='Assertions for testing the characteristics of orca tables and columns',
-    long_description=long_description,
     author='UrbanSim Inc.',
     author_email='info@urbansim.com',
     url='https://github.com/urbansim/orca_test',
@@ -24,8 +11,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(exclude=['*.tests']),
     install_requires=[
